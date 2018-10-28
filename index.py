413a1d8afcf734717121ef86d27bd7b68e7622a3
root <root@DESKTOP-D38DJ8C.localdomain>
2018-10-28 09:23:24 +0200

    Add move function to the snake

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
+from __future__ import print_function
+import time
 from snake import *
 
 def clear_screen():
   print(chr(27) + "[2J")
 
 def print_at(x,y,text):
-  output = "\033[" + str(x) + ";" + str(y) + "H" + text;
-  print(output);
+  output = "\033[" + str(y) + ";" + str(x) + "H" + text
+  print(output, end="")
 
-s = Snake()
 clear_screen()
-print(s.__dict__)
-#print_at(s.head[0],s.head[1], "Hello")
+s = Snake(print_at)
+for i in range(10):
+  time.sleep(1)
+  print("") # I'm not sure why, but this line is needed if we want the sleep to work well
+  s.move(print_at)
 
--- snake.py
+++ snake.py
@@ -1,8 +1,15 @@
 import Queue
 
 class Snake:
-  def __init__(self):
-    self.head  = {x:50, y:50}
+  def __init__(self, print_at):
+    self.head  = {"x":20, "y":20}
     self.body = Queue.Queue()
+    self.direction = {"x":1,"y":0}
+    print_at(self.head["x"], self.head["y"], "X")
 
-print("AAAAAAAAAAAAAA")
+  def move(self, print_at):
+    self.body.put(self.head)
+    self.head = {"x":self.head["x"] + self.direction["x"], "y":self.head["y"] + self.direction["y"]}
+    tail = self.body.get()
+    print_at(tail["x"], tail["y"], " ")
+    print_at(self.head["x"], self.head["y"], "X")
