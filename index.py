27b0067f68b8d1afaf5e5a7931ebf08fb7aa323e
root <root@DESKTOP-D38DJ8C.localdomain>
2018-10-28 09:39:08 +0200

    Add the option to change direction and to grow

--- index.py
+++ index.py
@@ -9,10 +9,22 @@ def print_at(x,y,text):
   output = "\033[" + str(y) + ";" + str(x) + "H" + text
   print(output, end="")
 
+
+def move10Times(s):
+  for i in range(10):
+    time.sleep(0.1)
+    print("") # I'm not sure why, but this line is needed if we want the sleep to work well
+    s.move(print_at)
+
 clear_screen()
 s = Snake(print_at)
-for i in range(10):
-  time.sleep(1)
-  print("") # I'm not sure why, but this line is needed if we want the sleep to work well
-  s.move(print_at)
 
+move10Times(s)
+s.change_direction('up')
+move10Times(s)
+s.change_direction('left')
+move10Times(s)
+s.change_direction('down')
+move10Times(s)
+s.change_direction('right')
+move10Times(s)
--- snake.py
+++ snake.py
@@ -5,7 +5,24 @@ class Snake:
     self.head  = {"x":20, "y":20}
     self.body = Queue.Queue()
     self.direction = {"x":1,"y":0}
+    self.initialize(print_at)
+
+  def grow(self, print_at):
+    self.body.put(self.head)
+    self.head = {"x":self.head["x"] + self.direction["x"], "y":self.head["y"] + self.direction["y"]}
     print_at(self.head["x"], self.head["y"], "X")
+  
+  def initialize(self, print_at):
+    BASIC_SIZE = 10
+    for i in range(BASIC_SIZE):
+      self.grow(print_at)
+
+  def change_direction(self, newDirection):
+      self.direction =  {'right': {"x":1,"y":0},
+                         'left':  {"x":-1,"y":0},
+                         'up': {"x":0,"y":-1},
+                         'down': {"x":0,"y":1}}.get(newDirection, self.direction)
+        
 
   def move(self, print_at):
     self.body.put(self.head)
