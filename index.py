594a997d52bb1b8bb3bf414efe940a23a4fe8346
root <root@DESKTOP-D38DJ8C.localdomain>
2018-10-28 11:40:43 +0200

    Add the ability to control the snake

--- index.py
+++ index.py
@@ -1,30 +1,44 @@
 from __future__ import print_function
 import time
 from snake import *
+import curses
 
-def clear_screen():
-  print(chr(27) + "[2J")
+MAP_HEIGHT = 100
+MAP_WIDTH = 100
+
 
 def print_at(x,y,text):
   output = "\033[" + str(y) + ";" + str(x) + "H" + text
   print(output, end="")
 
-
-def move10Times(s):
-  for i in range(10):
+def moveNTimes(s, N):
+  for i in range(N):
     time.sleep(0.1)
     print("") # I'm not sure why, but this line is needed if we want the sleep to work well
     s.move(print_at)
 
-clear_screen()
-s = Snake(print_at)
+def getDirection(key):
+  if key == "KEY_UP":
+    return 'up'
+  elif key == 'KEY_DOWN':
+    return 'down'
+  elif key == 'KEY_LEFT':
+    return 'left'
+  elif key == 'KEY_RIGHT':
+    return 'right'
+  return 'nothing'
 
-move10Times(s)
-s.change_direction('up')
-move10Times(s)
-s.change_direction('left')
-move10Times(s)
-s.change_direction('down')
-move10Times(s)
-s.change_direction('right')
-move10Times(s)
+def main(win):
+  win.nodelay(True)
+  key=""
+  s = Snake(print_at)
+  while 1:        
+    try:              
+      key = win.getkey()
+      s.change_direction(getDirection(str(key)))
+      moveNTimes(s,1)   
+    except Exception as e: 
+      # No input
+      moveNTimes(s,1)        
+
+curses.wrapper(main)
--- snake.py
+++ snake.py
@@ -7,15 +7,14 @@ class Snake:
     self.direction = {"x":1,"y":0}
     self.initialize(print_at)
 
-  def grow(self, print_at):
-    self.body.put(self.head)
-    self.head = {"x":self.head["x"] + self.direction["x"], "y":self.head["y"] + self.direction["y"]}
-    print_at(self.head["x"], self.head["y"], "X")
+  def grow(self):
+    self.shouldGrow = True # In the next move the snake will grow
   
   def initialize(self, print_at):
     BASIC_SIZE = 10
     for i in range(BASIC_SIZE):
-      self.grow(print_at)
+      self.grow()
+      self.move(print_at)
 
   def change_direction(self, newDirection):
       self.direction =  {'right': {"x":1,"y":0},
@@ -23,10 +22,12 @@ class Snake:
                          'up': {"x":0,"y":-1},
                          'down': {"x":0,"y":1}}.get(newDirection, self.direction)
         
-
   def move(self, print_at):
     self.body.put(self.head)
     self.head = {"x":self.head["x"] + self.direction["x"], "y":self.head["y"] + self.direction["y"]}
-    tail = self.body.get()
-    print_at(tail["x"], tail["y"], " ")
+    if (self.shouldGrow):
+      self.shouldGrow = False
+    else:
+      tail = self.body.get()
+      print_at(tail["x"], tail["y"], " ")
     print_at(self.head["x"], self.head["y"], "X")
